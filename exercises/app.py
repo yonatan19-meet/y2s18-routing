31fe1dfc75d525a7fb83dec1b528ce6414098a3a
aboud20-meet <aboud20@meet.mit.edu>
2018-08-08 12:17:07 +0300

    yes no maybe

--- exercises/app.py
+++ exercises/app.py
@@ -1,9 +1,19 @@
 from databases import *
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
-
 @app.route('/')
 def home():
-    return 'START HERE: CHANGE ME'
+	return render_template('page.html')
+
+@app.route('/student')
+def student():
+	return render_template('my_student.html')
+
+@app.route('/student/<int:student_id>')
+def display_student(student_id):
+    return render_template('student.html', student_id=student_id)
+
+
+
 
 app.run(debug=True)
